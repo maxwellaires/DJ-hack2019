5d447c75c1fe44087169bd187e521d230b4a12ea
Maxwell Aires <maxwellaires@gmail.com>
2019-02-08 23:28:00 -0500

    Working button

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
+#!/usr/bin/python
+
 import os
 
-f = open("/var/www/html/index.html","rt") 
-website = f.read()
-f.close()
-
-
+info = ["Yellow Submarine","Boulevard of Broken Dreams","THRILLER","Fireworks"]
+ratings = [0,0,0,0]
 
 def main(data):
+	f = open("/var/www/html/index.html","rt")
+	website = f.read()
+	f.close()
 
-    print("Content-type: text/html\n\n")
-    if data==None:
-        website = website.replace("??","Matthew's World'")
-    else:
-        pass
-        #do other stuff
+	print("Content-type: text/html\n\n")
+	if data==None:
+		pass
+	else:
+		if data=="b2=UP":
+			ratings[2] += 1
+#		pass
 
-
-    print(website)
+	website = website.replace("?1",info[0] + " " + str(ratings[0]))
+	website = website.replace("?2",info[1] + " " + str(ratings[1]))
+	website = website.replace("?3",info[2] + " " + str(ratings[2]))
+	website = website.replace("?4",info[3] + " " + str(ratings[3]))
+	print(website)
 
 
 if __name__ == "__main__":
     
-    if os.getenv("QUERY_STRING"):
-        data = os.getenv("QUERY_STRING")
-    else:
-        data = None
+	if os.getenv("QUERY_STRING"):
+		data = os.getenv("QUERY_STRING")
+	else:
+		data = None
 
-    main(data)
+	main(data)
 

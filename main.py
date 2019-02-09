9f8e101b606399d3a7b1431277ab7118f685e939
mmcqd <mmcquaid@andrew.cmu.edu>
2019-02-09 00:48:43 -0500

    fixed main and json

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ songs = ["Yellow Submarine","Boulevard of Broken Dreams","THRILLER","Fireworks"]
 
 
 def buildRatings(songs):
-    ratings = dict([(song,0) for song in songs]))
+    ratings = dict([(song,0) for song in songs])
     f = open("info.json","wt")
-    f.write(json.dumps(info))
+    f.write(json.dumps(ratings))
     f.close()
     return ratings
 
@@ -17,38 +17,36 @@ def buildRatings(songs):
 
 
 def main(data):
-	f = open("/var/www/html/index.html","rt")
-	website = f.read()
-	f.close()
+        f = open("/var/www/html/index.html","rt")
+        website = f.read()
+        f.close()
 
         f = open("info.json","rt")
         ratings = f.read()
         f.close()
         
-        if ratings = "":
+        if ratings == "":
             ratings = buildRatings(songs)
         else:
             ratings = json.loads(ratings)
 
-	print("Content-type: text/html\n\n")
-	if data==None:
-		pass
-	else:
-		if data=="b2=UP":
-			ratings[2] += 1
-#		pass
+        print("Content-type: text/html\n\n")
+        if data==None:
+                pass
+        else:
+                if data=="b2=UP":
+                        ratings[2] += 1
 
-	
-        for 
+        
         print(website)
 
 
 if __name__ == "__main__":
     
-	if os.getenv("QUERY_STRING"):
-		data = os.getenv("QUERY_STRING")
-	else:
-		data = None
+        if os.getenv("QUERY_STRING"):
+                data = os.getenv("QUERY_STRING")
+        else:
+                data = None
 
-	main(data)
+        main(data)
 
